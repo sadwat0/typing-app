79dedee71d391013d59f82853047a996c2db90fe
sadwat0 <ivanlarin001@gmail.com>
2024-04-04 21:11:57 +0300

    Fix tests & add settings tests

--- tests/test_typing_test.py
+++ tests/test_typing_test.py
@@ -1,162 +1,101 @@
-import re
-import random
 import pytest
-from src.text_generator import TextGenerator
-from src.typing_test import TypingTest, LetterColor
-from src import constants
-from src.statistics_classes import Statistics
-import sys
-import copy
 import flet as ft
+from unittest.mock import patch
+from src.typing_test import TypingTest, LetterColor
 
 
-def fill_page_content(page: ft.Page, typing_test: TypingTest):
-    """Adds some elements to page for default work of typing test"""
+@pytest.fixture
+def typing_test_mock(monkeypatch):
+    class MockControl:
+        def __init__(self):
+            self.controls = []
 
-    page.views.append(
-        ft.View(
-            "/",
-            [
-                ft.Row(
-                    [
-                        ft.ElevatedButton(
-                            "Statistics", on_click=lambda _: page.go("/stats")
-                        ),
-                        ft.ElevatedButton(
-                            "Heatmaps", on_click=lambda _: page.go("/heatmaps")
-                        ),
-                    ],
-                    alignment=ft.MainAxisAlignment.CENTER,
-                ),
-                ft.Divider(),
-                typing_test.visual_element,
-            ],
-            bgcolor=constants.color_scheme["background"],
-            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-        )
-    )
-    page.update()
+        def update(self):
+            pass
 
+    class MockPage:
+        """A mock implementation of the flet.Page class for testing purposes."""
 
-def get_typing_test(page: ft.Page) -> TypingTest:
+        def __init__(self):
+            self.views = [MockControl() for _ in range(5)]
+
+        def go(self, path):
+            pass
+
+    def mock_update(_):
+        pass
+
+    monkeypatch.setattr("flet.Control.update", mock_update)
+    page = MockPage()
     typing_test = TypingTest(page)
-    fill_page_content(page, typing_test)
 
     return typing_test
 
 
-def test_stop():
-    def test(page: ft.Page):
-        typing_test = get_typing_test(page)
-
-        # Test begin
-        typing_test.start()
-        typing_test.stop()
-        assert typing_test.status == typing_test.TestStatus.ENDED
-
-        page.window_destroy()
-
-    ft.app(target=test)
+def test_stop(typing_test_mock):
+    typing_test_mock.start()
+    typing_test_mock.stop()
+    assert typing_test_mock.status == typing_test_mock.TestStatus.ENDED
 
 
-def test_settings_changes():
-    def test(page: ft.Page):
-        typing_test = get_typing_test(page)
+def test_settings_changes(typing_test_mock):
+    old_punctuation = typing_test_mock.text_generator.punctuation
+    typing_test_mock.toggle_punctuation()
+    assert typing_test_mock.text_generator.punctuation != old_punctuation
 
-        # Test begin
-        old_punctuation = typing_test.text_generator.punctuation
-        typing_test.toggle_punctuation()
-        assert typing_test.text_generator.punctuation != old_punctuation
+    old_numbers = typing_test_mock.text_generator.numbers
+    typing_test_mock.toggle_numbers()
+    assert typing_test_mock.text_generator.numbers != old_numbers
 
-        old_numbers = typing_test.text_generator.numbers
-        typing_test.toggle_numbers()
-        assert typing_test.text_generator.numbers != old_numbers
+    typing_test_mock.select_time()
+    assert typing_test_mock.size_mode == "time"
+    assert typing_test_mock.available_time > 0
 
-        typing_test.select_time()
-        assert typing_test.size_mode == "time"
-        assert typing_test.available_time > 0
+    typing_test_mock.select_words()
+    assert typing_test_mock.size_mode == "words"
+    assert typing_test_mock.available_time is None
 
-        typing_test.select_words()
-        assert typing_test.size_mode == "words"
-        assert typing_test.available_time is None
-
-        typing_test.set_language(language="ru")
-        assert typing_test.language == "ru"
-
-        page.window_destroy()
-
-    ft.app(target=test)
+    typing_test_mock.set_language(language="ru")
+    assert typing_test_mock.language == "ru"
 
 
-def test_key_pressed_starts_test():
-    def test(page: ft.Page):
-        typing_test = get_typing_test(page)
+def test_key_pressed_starts_test(typing_test_mock):
+    assert typing_test_mock.status == TypingTest.TestStatus.NOT_STARTED
 
-        # Test begin
-        assert typing_test.status == TypingTest.TestStatus.NOT_STARTED
+    event = ft.KeyboardEvent("a", False, False, False, False)
+    typing_test_mock.key_pressed(event)
 
-        event = ft.KeyboardEvent("a", False, False, False, False)
-        typing_test.key_pressed(event)
-
-        assert typing_test.status == TypingTest.TestStatus.RUNNING
-
-        page.window_destroy()
-
-    ft.app(target=test)
+    assert typing_test_mock.status == TypingTest.TestStatus.RUNNING
 
 
-def test_key_pressed_correct_key():
-    def test(page: ft.Page):
-        typing_test = get_typing_test(page)
+def test_key_pressed_correct_key(typing_test_mock):
+    typing_test_mock.correct_text = "hello"
+    typing_test_mock.printed_text = "he"
 
-        # Test begin
-        typing_test.correct_text = "hello"
-        typing_test.printed_text = "he"
+    event = ft.KeyboardEvent("l", False, False, False, False)
+    typing_test_mock.key_pressed(event)
 
-        event = ft.KeyboardEvent("l", False, False, False, False)
-        typing_test.key_pressed(event)
-
-        assert typing_test.printed_text == "hel"
-        assert typing_test.letter_colors[2] == LetterColor.CORRECT
-
-        page.window_destroy()
-
-    ft.app(target=test)
+    assert typing_test_mock.printed_text == "hel"
+    assert typing_test_mock.letter_colors[2] == LetterColor.CORRECT
 
 
-def test_key_pressed_wrong_key():
-    def test(page: ft.Page):
-        typing_test = get_typing_test(page)
+def test_key_pressed_wrong_key(typing_test_mock):
+    typing_test_mock.correct_text = "hello"
+    typing_test_mock.printed_text = "he"
 
-        # Test begin
-        typing_test.correct_text = "hello"
-        typing_test.printed_text = "he"
+    event = ft.KeyboardEvent("a", False, False, False, False)
+    typing_test_mock.key_pressed(event)
 
-        event = ft.KeyboardEvent("a", False, False, False, False)
-        typing_test.key_pressed(event)
-
-        assert typing_test.printed_text == "hea"
-        assert typing_test.letter_colors[2] == LetterColor.WRONG
-
-        page.window_destroy()
-
-    ft.app(target=test)
+    assert typing_test_mock.printed_text == "hea"
+    assert typing_test_mock.letter_colors[2] == LetterColor.WRONG
 
 
-def test_backspace_pressed():
-    def test(page: ft.Page):
-        typing_test = get_typing_test(page)
+def test_backspace_pressed(typing_test_mock):
+    typing_test_mock.correct_text = "hello"
+    typing_test_mock.printed_text = "hea"
 
-        # Test begin
-        typing_test.correct_text = "hello"
-        typing_test.printed_text = "hea"
+    event = ft.KeyboardEvent("backspace", False, False, False, False)
+    typing_test_mock.key_pressed(event)
 
-        event = ft.KeyboardEvent("backspace", False, False, False, False)
-        typing_test.key_pressed(event)
-
-        assert typing_test.printed_text == "he"
-        assert typing_test.letter_colors[2] == LetterColor.UNUSED
-
-        page.window_destroy()
-
-    ft.app(target=test)
+    assert typing_test_mock.printed_text == "he"
+    assert typing_test_mock.letter_colors[2] == LetterColor.UNUSED
